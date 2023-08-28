2af095d7c116dcd75ffe4e3ee7117c0317500c1f
unknown <shafiul2494@gmail.com>
2023-08-28 21:15:15 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,17 @@
+// Navigation
+
+function openMenu(){
+    document.getElementById("menu-container").style.width = "100%";
+}
+
+
+function closeMenu(){
+    document.getElementById("menu-container").style.width = "0";
+}
+
+// MiXER 
+
+
+var mixer = mixitup('.work-grid');
+
+
